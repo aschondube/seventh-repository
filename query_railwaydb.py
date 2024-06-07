e0876bf674e46c1abfea09c314ef4c399f228db4
aschondube <schondubeandres@gmail.com>
2024-06-07 15:18:26 +0200

    Uploading file with code to perform queries to railwaydb

---
+++ query_railwaydb.py
@@ -0,0 +1,29 @@
+# code to perform queries on the cloud railway postgresql db (query in case is a row deletion on a date condition) 
+
+import os
+import psycopg2
+from psycopg2 import sql
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+path = os.getenv('db_url1')
+
+
+with psycopg2.connect(path) as conn:
+    cur = conn.cursor()
+    try:
+        delete_query = sql.SQL("DELETE FROM btc_prices WHERE date = %s")
+        cur.execute(delete_query, ('2023-06-06',))
+        rows_deleted = cur.rowcount
+        conn.commit()
+        print("Deletion successful")
+    except (psycopg2.OperationalError, psycopg2.ProgrammingError) as error:
+        print(f"Error deleting data: {error}")
+    if rows_deleted > 0:
+        print(f"{rows_deleted} row(s) deleted successfully")
+    else:
+        print("No rows found for deletion")
+
+conn.close()
